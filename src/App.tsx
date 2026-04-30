a0ca05362142b0d3eca7ec410637277de33a6d11
Huxley-xu <she092@connect.hkust-gz.edu.cn>
2026-04-30 20:28:45 +0800

    Fix scrolling: add min-h-0 to flex-1 overflow-y-auto panels

--- src/App.tsx
+++ src/App.tsx
@@ -769,7 +769,7 @@ export default function App() {
             )}
           </AnimatePresence>
 
-          <div className="flex-1 p-6 flex flex-col gap-6 overflow-y-auto">
+          <div className="flex-1 min-h-0 p-6 flex flex-col gap-6 overflow-y-auto">
             {selectedPatient ? (
               <>
                 {/* Top: Clinical Summary Grid */}
@@ -1389,7 +1389,7 @@ export default function App() {
             </div>
           </div>
 
-          <div className="flex-1 overflow-y-auto custom-scrollbar p-6 space-y-8">
+          <div className="flex-1 min-h-0 overflow-y-auto custom-scrollbar p-6 space-y-8">
             {/* 1. Question Construction Score */}
             <div className="space-y-4">
                <ScoreDimension 
